b33ca4023e3754e02c9d6b28750c7ffba410a70c
joanaportugal <46449775+joanaportugal@users.noreply.github.com>
2023-04-17 13:42:08 +0100

    technologies

---
+++ src/controllers/Technology.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { ValidationError } from "sequelize";
+
+import { tables as db } from "../database";
+
+class TechnologyController {
+	async getAll(req: Request, res: Response) {
+		try {
+			const technologies = await db.Technology.findAll({ order: [["technology_id", "ASC"]] });
+			return res.status(200).json({ technologies });
+		} catch (err) {
+			return res.status(500).json({
+				error: "An error occurred. Try again!",
+			});
+		}
+	}
+
+	async createOne(req: Request, res: Response) {
+		try {
+			await db.Technology.create({ technology_name: req.body.name });
+
+			return res.status(200).json({ message: "Technology created." });
+		} catch (err) {
+			if (err instanceof ValidationError) {
+				return res.status(400).json({
+					msg: err.errors.map((e) => e.message),
+				});
+			}
+			return res.status(500).json({
+				error: "An error occurred. Try again!",
+			});
+		}
+	}
+}
+
+export default TechnologyController;
